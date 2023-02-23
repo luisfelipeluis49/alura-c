4b3edb11c82c7b398dd6e77eb96b6df1cbfbb65a
luisfelipeluis49 <luisfelipeluis49@gmail.com>
2023-02-22 23:33:58 -0300

    deasfio multiplicacao

--- advinhacao/program.c
+++ advinhacao/program.c
@@ -2,9 +2,29 @@
 
 int main ()
 {
+  int x, y;
+
+  printf("Digite os fatores X e Y para a multiplicação\n");
+  printf("X: "); scanf("%d", &x);
+  printf("Y: "); scanf("%d", &y);
+  int resultado = x * y;
+  printf("O resultado é: %d \n", resultado);
+
+
   printf("***************************************\n");
   printf("*               Welcome               *\n");
   printf("***************************************\n");
+
+  int secretNumber = 42;
+
+  int guess = 0;
+
+  do {
+    printf("What's your guess on the secret number? \n");
+    printf("Guess: "); scanf("%d", &guess);
+  }while (guess != secretNumber);
+   
+  printf("You're right!\n");
   return 0;
 }
 
